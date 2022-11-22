742bcd86330eafc89548c0f018dc33e5f1b4e174
ssarun9871 <ssarun9871@gmail.com>
2022-11-23 02:59:37 +0530

    task 13

--- appointment_app/test.js
+++ appointment_app/test.js
@@ -6,6 +6,10 @@ function addData() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
+    if(localStorage.getItem(email)!==null){
+        alert("This email id already registered !");
+        return;
+    }
 
     let obj ={
         name : name,
@@ -22,20 +26,46 @@ function addData() {
 
  
  for(let i=0;i<localStorage.length;i++){
-    let key=localStorage.key(i);
+    let key=localStorage.key(i);//it gives the key
     let data = localStorage.getItem(key);
     let dataName = JSON.parse(data).name;
+    let dataEmail = JSON.parse(data).email;
 
-    var user = document.createElement('li');
-    var text = document.createTextNode(dataName);
-
-    user.appendChild(text);
-    console.log(user);
+ 
+    var li = `<li id="${dataEmail}">${dataName} <button  onClick="deleteuser('${dataEmail}')">delete</button>
+    <button onClick="updateUser('${dataEmail}')">edit</button></li>`;
     let target = document.querySelector('.usersList');
-    
-    target.appendChild(user);
+    target.innerHTML = target.innerHTML + li;
+    console.log(li);
+ }
+
+ function deleteuser(em){
+    localStorage.removeItem(em);
+    deleteUserFromScreen(em);
 
  }
+ function deleteUserFromScreen(em){
+    let parentNode = document.querySelector('.usersList');
+    let childNode = document.getElementById(em);
+    parentNode.removeChild(childNode);
+ }
+
+
+ function updateUser(em){
+    let nam = JSON.parse(localStorage.getItem(em)).name;
+    let emai = JSON.parse(localStorage.getItem(em)).email;
+    let ph = JSON.parse(localStorage.getItem(em)).phone;
+    
+    document.getElementById('name').value = nam;
+    document.getElementById('email').value = emai;
+    document.getElementById('phone').value = ph;
+    
+    deleteUserFromScreen(em);
+ }
+
+
+
+  
   
 
 
