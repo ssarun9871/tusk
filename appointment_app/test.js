1cb4cb208f9ff45df635cad19b67f71c59fe3be6
ssarun9871 <ssarun9871@gmail.com>
2022-11-22 19:48:03 +0530

    task 11

--- appointment_app/test.js
+++ appointment_app/test.js
@@ -7,10 +7,17 @@ function addData() {
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
 
-
-    localStorage.setItem('Name', name);
-    localStorage.setItem('Email', email);
-    localStorage.setItem('Phone', phone);
+    let obj ={
+        name : name,
+        email : email,
+        phone : phone
+    };
+    
+    let myObj_serialized = JSON.stringify(obj);
+    localStorage.setItem("moObj",myObj_serialized);
+    // localStorage.setItem('Nam', name);
+    // localStorage.setItem('Email', email);
+    // localStorage.setItem('Phone', phone);
 }
 
 
